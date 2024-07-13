dcfdf0a5c2b3c135e6c276b7a033d766b571f009
mandarnaik016 <mandarnaik016@gmail.com>
2024-07-13 21:50:33 +0530

    Font Rectified Signed-off-by: mandarnaik016 <mandarnaik016@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,4 @@
 var fontawesome = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css';
-var lora = 'https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&display=swap';
-var sans = 'https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&display=swap';
-
-Defer.css(lora, {crossorigin: 'anonymous'}, 0, function() {
-    console.info('Lora is loaded!!!');
-});
-
-Defer.css(sans, {crossorigin: 'anonymous'}, 0, function() {
-    console.info('Sans is loaded!!!');
-});
 
 Defer.css(fontawesome, {crossorigin: 'anonymous'}, 0, function() {
     console.info('FontAwesome is loaded!!!');
