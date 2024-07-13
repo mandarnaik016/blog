7298ad2e87ece5dc287bf1149a76518a62ce32cc
mandarnaik016 <mandarnaik016@gmail.com>
2024-07-13 21:34:54 +0530

    Font awesome lazy load Signed-off-by: mandarnaik016 <mandarnaik016@gmail.com>

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+var fontawesome = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css';
+Defer.css(fontawesome, {crossorigin: 'anonymous'}, 0, function() {
+    console.info('FontAwesome is loaded.');
+});
